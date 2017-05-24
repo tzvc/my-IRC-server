f608355bf379a97cab2b3f805da34144b3e3c2de
bufferking <bufferking@defense.gov>
2017-05-24 16:15:15 +0200

    user_manager v.1.0

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 ## Login   <theo.champion@epitech.eu>
 ## 
 ## Started on  Wed May 24 15:39:32 2017 theo champion
-## Last update Wed May 24 15:41:22 2017 theo champion
+## Last update Wed May 24 16:14:50 2017 
 ##
 
 CC	= gcc
@@ -21,6 +21,7 @@ NAME	= server
 
 SRCS	= $(SRCDIR)/main.c		\
           $(SRCDIR)/socket.c		\
+	  $(SRCDIR)/user_manager.c
 
 OBJS	= $(SRCS:.c=.o)
 
---
+++ src/user_manager.c
@@ -0,0 +1,76 @@
+/*
+** client_manager.c for PSU_2016_myirc in /home/bufferking/rendu/network/PSU_2016_myirc
+** 
+** Made by bufferking
+** Login   <antoine.cauquil@epitech.eu>
+** 
+** Started on  Wed May 24 15:01:09 2017 bufferking
+** Last update Wed May 24 16:14:15 2017 
+*/
+
+#include "myirc.h"
+
+size_t		count_users(t_user **users)
+{
+  size_t	i;
+  t_user	*tmp;
+  
+  i = 0;
+  tmp = *users;
+  while (tmp)
+    {
+      printf("listed user : %s\n", tmp->nick);
+      tmp = tmp->next;
+      i++;
+    }
+  return (i);
+}
+
+int		del_user(t_user **users, t_user *old)
+{
+  t_user	*tmp;
+  
+  tmp = *users;
+  if (!tmp)
+    return (-1);
+  if (tmp == old)
+    {
+      *users = tmp->next;
+      free(tmp);
+      return (0);
+    }
+  while (tmp)
+    {
+      if (tmp->next == old)
+	{
+	  tmp->next = old->next;
+	  free(old);
+	  return (0);
+	}
+      tmp = tmp->next;
+    }
+  return (-1);
+}
+
+int		new_user(t_user **users, int fd, char *nick, char *host)
+{
+  t_user	*new;
+  t_user	*tmp;
+  
+  if (!(new = malloc(sizeof(t_user))))
+    return (-1);
+  new->fd = fd;
+  new->nick = nick;
+  new->host = host;
+  new->next = NULL;
+  if (!(*users))
+      *users = new;
+  else
+    {
+      tmp = *users;
+      while (tmp->next)
+	tmp = tmp->next;
+      tmp->next = new;
+    }
+  return (0);
+}
