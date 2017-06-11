c4d24f8e5feb983e5a5308e6311dba0cc6e20861
Théo champion <theo.champion@epitech.eu>
2017-06-11 22:13:43 +0200

    fix leak

--- src_server/free.c
+++ src_server/free.c
@@ -5,7 +5,7 @@
 ** Login   <theo.champion@epitech.eu>
 ** 
 ** Started on  Tue Jun  6 15:34:47 2017 theo champion
-** Last update Tue Jun  6 15:35:49 2017 theo champion
+** Last update Sun Jun 11 22:11:39 2017 theo champion
 */
 
 #include "irc_server.h"
@@ -54,6 +54,16 @@ void		free_all_chans(t_chan **chans)
 
 void		free_chan(t_chan *chan)
 {
+  t_user	*tmp;
+  t_user	*prev;
+
+  tmp = chan->users;
+  while (tmp)
+    {
+      prev = tmp;
+      tmp = tmp->next;
+      free(prev);
+    }
   if (chan->name)
     free(chan->name);
   if (chan->topic)
